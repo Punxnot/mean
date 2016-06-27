6ebaf0f33736742c9f3c3dd9bec2befa19a84c66
Marina <do-you-speak@yandex.ru>
2016-06-28 01:04:02 +0300

    Add authorization, fix error with upvotes

--- app.js
+++ app.js
@@ -8,9 +8,16 @@ var bodyParser = require('body-parser');
 var app = express();
 
 var mongoose = require('mongoose');
+
+var passport = require('passport');
+
 mongoose.connect('mongodb://localhost/news');
+
 require('./models/Posts');
 require('./models/Comments');
+require('./models/Users');
+
+require('./config/passport');
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
@@ -26,6 +33,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(passport.initialize());
 
 app.use('/', routes);
 app.use('/users', users);
